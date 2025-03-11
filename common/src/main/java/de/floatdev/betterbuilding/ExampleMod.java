701164f6186d395028e58457273864c48ede10ce
Alexander Neitzel <alexander.neitzel@garmingo.com>
2025-03-11 12:50:01 +0100

    Switched to Architectury Template

---
+++ common/src/main/java/de/floatdev/betterbuilding/ExampleMod.java
@@ -0,0 +1,9 @@
+package de.floatdev.betterbuilding;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "create_better_building";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}

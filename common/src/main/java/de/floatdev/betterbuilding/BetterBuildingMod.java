64dadbf002abd7805ab6ec88b6954253a31259c4
Alexander Neitzel <alexander.neitzel@garmingo.com>
2025-03-11 15:00:03 +0100

    Update copyright information and add GPL3 license to mod files

--- common/src/main/java/de/floatdev/betterbuilding/BetterBuildingMod.java
+++ common/src/main/java/de/floatdev/betterbuilding/BetterBuildingMod.java
@@ -1,12 +1,36 @@
 /*
- *   Copyright (c) 2025 Garmingo UG (haftungsbeschraenkt)
- *   All rights reserved.
- *   Unauthorized use, reproduction, and distribution of this source code is strictly prohibited.
+ *   Copyright (c) 2025 Alexander Neitzel (TheRealFloatDev)
+
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package de.floatdev.betterbuilding;
+
+import dev.architectury.registry.CreativeTabRegistry;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
 public final class BetterBuildingMod {
     public static final String MOD_ID = "create_better_building";
+
+    public static final CreativeModeTab MY_TAB = CreativeTabRegistry.create(
+            new ResourceLocation(MOD_ID, Component.translatable(MOD_ID + ".tab").getString()), // Tab ID
+            () -> new ItemStack(Items.STONE) // Icon
+    );
 
     public static void init() {
         // Write common init code here.
--- fabric/src/main/java/de/floatdev/betterbuilding/fabric/BetterBuildingModFabric.java
+++ fabric/src/main/java/de/floatdev/betterbuilding/fabric/BetterBuildingModFabric.java
@@ -1,8 +1,20 @@
 /*
- *   Copyright (c) 2025 Garmingo UG (haftungsbeschraenkt)
- *   All rights reserved.
- *   Unauthorized use, reproduction, and distribution of this source code is strictly prohibited.
+ *   Copyright (c) 2025 Alexander Neitzel (TheRealFloatDev)
+
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package de.floatdev.betterbuilding.fabric;
 
 import net.fabricmc.api.ModInitializer;

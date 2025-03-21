798b3bdd0793b278720cb44d968eeb6afa114c77
Alexander Neitzel <alexander.neitzel@garmingo.com>
2025-03-21 10:20:22 +0100

    Add color mappings and integrate into block translations generation

---
+++ scripts/colors.js
@@ -0,0 +1,35 @@
+/*
+ *   Copyright (c) 2025 Alexander Neitzel (TheRealFloatDev)
+
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+export const COLOR_MAPPINGS = {
+    "white": "White",
+    "light_gray": "Light Gray",
+    "gray": "Gray",
+    "black": "Black",
+    "brown": "Brown",
+    "red": "Red",
+    "orange": "Orange",
+    "yellow": "Yellow",
+    "lime": "Lime",
+    "green": "Green",
+    "cyan": "Cyan",
+    "light_blue": "Light Blue",
+    "blue": "Blue",
+    "purple": "Purple",
+    "magenta": "Magenta",
+    "pink": "Pink",
+};
